45b180392b7a4ad23f8576aefbed14fb46aa7200
Kounosuke <shiina04jbs10@gmail.com>
2025-09-05 14:41:08 +0900

    Add files via upload

---
+++ Assets/Player_move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    private void Update()
+    {
+        if (IsOwner == false)
+        {
+            return;
+        }
+
+        Vector2 direction = new Vector2()
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = Input.GetAxisRaw("Vertical")
+        };
+        float moveSpeed = 3f;
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Ui_Manager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+public class Ui_Manager : MonoBehaviour
+{
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button clientButton;
+    private void Awake()
+    {
+        hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+        clientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
